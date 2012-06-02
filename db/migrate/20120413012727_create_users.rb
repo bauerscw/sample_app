a62f473ead10ac222127dce14b7d69a99cf7cf54
Cody Bauers <cbauers@ubuntu.(none)>
2012-06-02 17:11:34 -0400

    user tests

--- db/migrate/20120413012727_create_users.rb
+++ db/migrate/20120413012727_create_users.rb
@@ -1,10 +1,5 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-
-      t.timestamps
-    end
+    add_index :users, :email, unique: true
   end
 end
