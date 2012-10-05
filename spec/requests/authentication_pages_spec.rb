4ebdde4ed5054b619f0f044010ce432006668a81
Cody Bauers <cbauers@ubuntu.(none)>
2012-10-04 22:04:44 -0400

    Stay signed in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,6 +5,19 @@ describe "Authentication" do
 
   describe "signin page" do
     before { visit signin_path }
+
+    describe "signin" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+
+    end
     it { should have_selector('h1', text: 'Sign in') }
     it { should have_selector('title', text: 'Sign in') }
   end
