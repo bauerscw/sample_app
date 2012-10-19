fe800dc996971267e0554eebc2e94d5f44120f26
Cody Bauers <cbauers@ubuntu.(none)>
2012-10-18 22:17:24 -0400

    more signin cookies

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
+  root to: 'static_pages#home'
+
   match '/signup', to: 'users#new'
   match '/signin', to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,21 @@ require 'spec_helper'
 describe "User pages" do
   subject { page }
 
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1', text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
   describe "signup" do
     before { visit signup_path }
 
@@ -32,35 +47,15 @@ describe "User pages" do
           click_button "Create my account"
         end.to change(User, :count).by(1)
       end
+
       describe "after saving the user" do
         before { click_button "Create my account" }
         let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
-  end
-
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_selector('h1', text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
-  end
-
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_selector('h1', text: user.name) }
-    it { should have_selector('title', text: user.name) }
-  end
-
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
   end
 end
