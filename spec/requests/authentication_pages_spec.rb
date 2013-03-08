205ebb9275a27cd84ce3e2aefbf52657fcd52c5c
Cody Bauers <cbauers@ubuntu.(none)>
2013-03-07 21:48:25 -0500

    pagination

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,6 +18,7 @@ describe "Authentication" do
       before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
@@ -35,6 +36,21 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email", with: user.email
+          fill_in "Password", with user.password
+          click_button "Sign in"
+        end
+        
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end
+      end
+
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -45,6 +61,11 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { respect.should redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
         end
       end
     end
