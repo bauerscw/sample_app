8d7bdabe16fdf7fdc753b97acbbea2f71da81cde
Cody Bauers <cbauers@ubuntu.(none)>
2013-05-16 21:39:20 -0400

    10.2 displaying microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,7 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
 
   validates :user_id, presence: true
+  validates :user_id, presence: true
+
+  default_scope order: 'microposts.created_at DESC'
 end
